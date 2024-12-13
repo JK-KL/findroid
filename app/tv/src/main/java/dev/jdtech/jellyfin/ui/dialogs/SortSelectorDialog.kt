7312a6edc76456cddfad81703e35a8b106b1ff05
T0T <zjl.works@gmail.com>
2024-12-13 14:21:41 +0800

    1. enable sort

--- app/tv/src/main/java/dev/jdtech/jellyfin/ui/dialogs/SortSelectorDialog.kt
+++ app/tv/src/main/java/dev/jdtech/jellyfin/ui/dialogs/SortSelectorDialog.kt
@@ -2,7 +2,6 @@ package dev.jdtech.jellyfin.ui.dialogs
 
 import android.os.Parcelable
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -15,10 +14,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
